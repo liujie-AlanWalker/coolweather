ce6ce1e9b3a39e5d28e6400a3e400cb213718e26
liujie-AlanWalker <425724056@qq.com>
2018-11-29 16:43:17 +0800

    加入显示天气信息的功能

---
+++ app/src/main/java/com/example/coolweather/gson/AQI.java
@@ -0,0 +1,17 @@
+package com.example.coolweather.gson;
+
+/**
+ * Created by Administrator on 2018/11/28.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+    }
+}
--- app/src/main/java/com/example/coolweather/util/Utility.java
+++ app/src/main/java/com/example/coolweather/util/Utility.java
@@ -5,6 +5,8 @@ import android.text.TextUtils;
 import com.example.coolweather.db.City;
 import com.example.coolweather.db.County;
 import com.example.coolweather.db.Province;
+import com.example.coolweather.gson.Weather;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -16,8 +18,8 @@ import org.json.JSONObject;
 
 public class Utility {
 
-    /*
-        解析和处理服务器返回的省级数据
+    /**
+     * 解析和处理服务器返回的省级数据
      */
     public static boolean handleProvinceResponse(String response) {
 
@@ -39,8 +41,8 @@ public class Utility {
         return false;
     }
 
-    /*
-        解析和处理服务器返回的市级数据
+    /**
+     * 解析和处理服务器返回的市级数据
      */
     public static boolean handleCityResponse(String response , int provinceId) {
 
@@ -63,8 +65,8 @@ public class Utility {
         return false;
     }
 
-    /*
-        解析和处理服务器返回的县级数据
+    /**
+     * 解析和处理服务器返回的县级数据
      */
     public static boolean handleCountyResponse(String response , int cityId) {
 
@@ -86,4 +88,21 @@ public class Utility {
         }
         return false;
     }
+
+    /**
+     * 将返回的JSON数据解析成Weather实体类
+     */
+    public static Weather handleWeatherResponse(String response) {
+
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent , Weather.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
